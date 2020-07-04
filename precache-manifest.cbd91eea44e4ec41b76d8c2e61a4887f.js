dad13c7f1b31df78a72486f1cc209a4c93e516f4
Icefox <kzasorin@gmail.com>
2020-07-04 18:42:03 +0300

    Updates

---
+++ precache-manifest.cbd91eea44e4ec41b76d8c2e61a4887f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86feb32c7d11960faedab5b5d898ee90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac3a50459057a1fdae1",
+    "url": "/static/css/main.7b61da9c.chunk.css"
+  },
+  {
+    "revision": "ec4c98356e360f1799e3",
+    "url": "/static/js/2.2efbbb36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2efbbb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac3a50459057a1fdae1",
+    "url": "/static/js/main.31d82bf9.chunk.js"
+  },
+  {
+    "revision": "fd5b1da98214a2642d7d",
+    "url": "/static/js/runtime-main.d33d1d1e.js"
+  }
+]);
